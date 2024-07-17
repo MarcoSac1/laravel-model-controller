d5dfcf15044efb9f54f4fa30ebe719fde45e9fa1
MarcoSac1 <marcosaccone8@gmail.com>
2024-07-17 17:08:42 +0200

    loaded db

--- app/Http/Controllers/Guest/MovieController.php
+++ app/Http/Controllers/Guest/MovieController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
@@ -11,7 +12,7 @@ class MovieController extends Controller
 
     public function index(){
         //prendo i film
-
+        $movies = Movie::all();
         return view('movie.index');
     }
 }
---
+++ resources/views/movie/index.blade.php
@@ -0,0 +1,7 @@
+@extends('app')
+
+@section('main-content')
+
+    <h1>Books</h1>
+    @dump($movies)
+@endsection
